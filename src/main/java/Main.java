a4c0c2365e398520ac903042a6df7502b86feb9d
lada <nicklada94@gmail.com>
2020-04-04 00:35:02 +0300

    Fixes #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,9 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+    long bonus = service.calculate(1000_60, true);
+    System.out.println (bonus);
 
-    service.calculate(amount, registered);
   }
 }
 
